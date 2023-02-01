47f23fc4cbd9c61e7b78dcc8bd661083effded1d
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2023-02-01 10:47:24 +0530

    first commit

---
+++ src/main/java/com/jenkins/JenkinsprogramApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsprogramApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jenkins/controller/JenkinsController.java
@@ -0,0 +1,16 @@
+package com.jenkins.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/get")
+public class JenkinsController {
+	
+	@RequestMapping(value="/message",produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getMessage() {
+		return "springboot jenkins working be happy";
+	}
+
+}
