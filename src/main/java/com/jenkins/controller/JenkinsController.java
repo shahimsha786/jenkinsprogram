461342ef0b2db8b87b363106d83b9f01d3f8475d
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2023-02-01 11:16:15 +0530

    my commit4

--- src/main/java/com/jenkins/controller/JenkinsController.java
+++ src/main/java/com/jenkins/controller/JenkinsController.java
@@ -1,14 +1,13 @@
 package com.jenkins.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/get")
+@RequestMapping(value = "/get")
 public class JenkinsController {
-	
-	@RequestMapping(value="/message",produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/message")
 	public String getMessage() {
 		return "springboot jenkins working be happy shahimsha";
 	}
